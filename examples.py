2d7436eebc5fa286c6a54e049ab12bf29faaafa9
Owen Mackwood <owen.mackwood@gmail.com>
2020-07-03 17:05:04 +0200

    Renamed Subplots to Panels

--- examples.py
+++ examples.py
@@ -1,35 +1,35 @@
 from typing import NamedTuple
 from pathlib import Path
 import tempfile
-from pubfig import FigureSpec, ElemSize, Units, ImageType, Location, SVG, Subplot, SubplotFig, Text
+from pubfig import FigureSpec, ElemSize, Units, ImageType, Location, SVG, Panel, PanelFig, Text
 from pubfig import compositor, spines_frames
 
 units = Units.cm
 
 
 class Figure1(FigureSpec):
-    class Subplots(NamedTuple):
+    class Panels(NamedTuple):
 
-        schematic: Subplot = Subplot(
+        schematic: Panel = Panel(
             SVG(Path("./schematic.svg")),
             Location(.25, .5),
             figure_offset=Location(.5, .5),
             text=Text("This is a schematic", 0, -.2, size=10, weight="bold", font="serif")
         )
 
-        noise_image: SubplotFig = SubplotFig(
+        noise_image: PanelFig = PanelFig(
             ElemSize(3., 3., units),
             Location(4.5, .5)
         )
 
-        time_series: SubplotFig = SubplotFig(
+        time_series: PanelFig = PanelFig(
             ElemSize(6, 4, units),
             Location(.25, 4.5),
             figure_offset=Location(.5, 0),
             gridspec_kwargs=dict(nrows=2, ncols=2, wspace=1., hspace=1)
         )
 
-    subplots = Subplots()
+    panels = Panels()
     figure_size = ElemSize(7.5, 9, units)
     svg_path = Path(tempfile.gettempdir()) / "figure_1.svg"
     plot_grid_every = 1
@@ -39,24 +39,24 @@ class Figure1(FigureSpec):
 
 @compositor(Figure1)
 def create_fig1(figure: Figure1):
-    subplot_noise_image(figure.subplots.noise_image)
-    subplot_time_series(figure.subplots.time_series)
+    plot_noise_image(figure.panels.noise_image)
+    plot_time_series(figure.panels.time_series)
 
 
-def subplot_noise_image(sp_fig: SubplotFig):
+def plot_noise_image(panel: PanelFig):
     import numpy as np
-    ax = sp_fig.fig.add_subplot(sp_fig.gridspec[0])
+    ax = panel.fig.add_subplot(panel.gridspec[0])
     ax.pcolor(np.random.uniform(size=50*50).reshape(50, 50))
     ax.set(xticks=(), yticks=())
 
 
-def subplot_time_series(sp_fig: SubplotFig):
+def plot_time_series(panel: PanelFig):
     import numpy as np
-    gs = sp_fig.gridspec
+    gs = panel.gridspec
     plots = gs.get_geometry()
     for k in range(np.product(plots)):
         i, j = np.unravel_index(k, plots)
-        ax = sp_fig.fig.add_subplot(gs[i, j])
+        ax = panel.fig.add_subplot(gs[i, j])
         ax.plot(np.cos(np.linspace(-k*np.pi, k*np.pi, 10**3)))
         ax.set(ylim=(-1.05, 1.05))
         spines_frames(ax)
--- pubfig.py
+++ pubfig.py
@@ -10,6 +10,7 @@ plt_ppi = 72  # Matplotlib defines 1 pt to be 1/72 of an inch
 cm_per_inch = 2.54
 scaling_magic = 4/3
 
+
 class Units(Enum):
     inch = 1
     cm = 2
@@ -89,7 +90,7 @@ class SVG:
             return Units.pt
 
 
-class Subplot:
+class Panel:
     def __init__(
             self,
             figure: Union[plt.Figure, SVG],
@@ -107,10 +108,10 @@ class Subplot:
         self.scale: float = scale
 
 
-class SubplotFig(Subplot):
+class PanelFig(Panel):
     def __init__(
             self,
-            subplot_size: ElemSize,
+            fig_size: ElemSize,
             location: Location,
             text: Optional[Union[Text, Tuple[Text, ...]]] = None,
             gridspec_kwargs: Optional[Dict[str, Any]] = None,
@@ -121,7 +122,7 @@ class SubplotFig(Subplot):
         gs_kwargs.setdefault("nrows", 1)
         gs_kwargs.setdefault("ncols", 1)
 
-        figure: plt.Figure = plt.figure(figsize=figure_size_in_inches(subplot_size))
+        figure: plt.Figure = plt.figure(figsize=figure_size_in_inches(fig_size))
         self.gridspec: GridSpec = figure.add_gridspec(**gs_kwargs)
 
         super().__init__(figure, location, text, auto_label, figure_offset)
@@ -135,18 +136,18 @@ class AutoLabelOptions(NamedTuple):
 
 
 class FigureSpec(SimpleNamespace):
-    class Subplots(NamedTuple):
+    class Panels(NamedTuple):
         """
-        Just a placeholder for the user definition of Subplots,
+        Just a placeholder for the user definition of Panels,
         so that we can specify that FigureSpec has
-        subplots: NamedTuple[Subplot, ...]
+        panels: NamedTuple[Panel, ...]
         which doesn't work due to a TypeError.
         """
-        subplot: Union[Subplot, SubplotFig]
+        panel: Union[Panel, PanelFig]
 
     svg_path: Union[Path, str]
     figure_size: ElemSize
-    subplots: Subplots
+    panels: Panels
     plot_grid_every: Union[float, int] = 0
     generate_image: ImageType = ImageType.none
     image_dpi: int = 400
@@ -156,9 +157,11 @@ class FigureSpec(SimpleNamespace):
 UserFigureSpec = TypeVar("UserFigureSpec")
 
 
-def compositor(figure_spec: Type[UserFigureSpec], delete_png: bool=False):
+def compositor(figure_spec: Type[UserFigureSpec], delete_png: bool = False):
     assert issubclass(figure_spec, FigureSpec), "The compositor needs the figure type: `@compositor(FigureSpec)`"
+
     def compositor_decorator(fn: Callable[..., Any]):
+
         def wrapped_fn(*args, **kwargs):
             fig_spec = figure_spec()
             result = fn(fig_spec, *args, **kwargs)
@@ -168,30 +171,30 @@ def compositor(figure_spec: Type[UserFigureSpec], delete_png: bool=False):
     return compositor_decorator
 
 
-def composite(figure: FigureSpec, delete_png=False):
+def composite(fig_spec: FigureSpec, delete_png=False):
     import tempfile
 
     tempdir = Path(tempfile.gettempdir())
     panels = []
-    if figure.plot_grid_every > 0:
-        pts_per_unit = get_pts_per_unit(figure.figure_size.units)
-        grid_every = figure.plot_grid_every * pts_per_unit
+    if fig_spec.plot_grid_every > 0:
+        pts_per_unit = get_pts_per_unit(fig_spec.figure_size.units)
+        grid_every = fig_spec.plot_grid_every * pts_per_unit
         panels.append(sc.Grid(grid_every, grid_every, size=0))
 
     label_n = 0
-    auto_label = figure.auto_label_options._asdict()
+    auto_label = fig_spec.auto_label_options._asdict()
     first_char = auto_label.pop("first_char")
-    for name in figure.subplots._fields:
-        subplot = getattr(figure.subplots, name)
-        assert isinstance(subplot, Subplot)
+    for name in fig_spec.panels._fields:
+        panel = getattr(fig_spec.panels, name)
+        assert isinstance(panel, Panel)
 
         panel_elements = []
 
-        assert isinstance(subplot.fig, (plt.Figure, SVG))
-        figure_offset = location_to_str(figure.figure_size.units, subplot.figure_offset)
-        if isinstance(subplot.fig, plt.Figure):
+        assert isinstance(panel.fig, (plt.Figure, SVG))
+        figure_offset = location_to_str(fig_spec.figure_size.units, panel.figure_offset)
+        if isinstance(panel.fig, plt.Figure):
             fn = tempdir / f"{name}.svg"
-            subplot.fig.savefig(fn, transparent=True)
+            panel.fig.savefig(fn, transparent=True)
             """
             Rescale required due to matplotlib saving SVG files with correct 'size',
             but with wrong scale. To see this, open the SVG in Inkscape, and change its 
@@ -202,53 +205,52 @@ def composite(figure: FigureSpec, delete_png=False):
             also have units of pt, but might actually be defaulting to 'px' (the user unit). 
             I find this all deeply confusing, and it is not at all clear why exactly this is, 
             but might related to the fact that 1 pt == 4/3 px, by some definitions (e.g. CSS).
-            Regardless, it causes the subplot-figure to appear at 3/4 of its
+            Regardless, it causes the panel-figure to appear at 3/4 of its
             actual size in the composite image.
             https://www.w3.org/TR/SVG11/coords.html#Units
             One thing to try might be to specify figures in pts, based on the default DPI
             used by matplotlib, or alter the default DPI to bring 1 pt == 1 px
             """
             svg = sc.SVG(fn)
-            if subplot.scale is None:
+            if panel.scale is None:
                 svg.scale(scaling_magic)  # Default scaling as per block comment above
             else:  # Custom scaling per user request
-                svg.scale(subplot.scale)
+                svg.scale(panel.scale)
             panel_elements.append(svg.move(*figure_offset))
         else:
-            scale = subplot.scale or subplot.fig.scale
-            print(f"Scaling SVG {subplot.fig.file} by {scale}")
-            panel_elements.append(subplot.fig.svg.scale(scale).move(*figure_offset))
+            scale = panel.scale or panel.fig.scale
+            print(f"Scaling SVG {panel.fig.file} by {scale}")
+            panel_elements.append(panel.fig.svg.scale(scale).move(*figure_offset))
 
-        if subplot.text is not None:
+        if panel.text is not None:
             panel_elements += [
                 sc.Text(
                     t.text,
-                    *location_to_str(figure.figure_size.units, Location(t.x, t.y, subplot.location.units)),
+                    *location_to_str(fig_spec.figure_size.units, Location(t.x, t.y, panel.location.units)),
                     **t.kwargs
                 ).move(*figure_offset)
-                for t in subplot.text]
+                for t in panel.text]
 
-        if subplot.auto_label:
+        if panel.auto_label:
             label = sc.Text(
                 f"{chr(ord(first_char) + label_n)}",
-                *location_to_str(figure.figure_size.units, Location(0, 0)),
+                *location_to_str(fig_spec.figure_size.units, Location(0, 0)),
                 **auto_label
             )
             label_n += 1
             panel_elements.append(label)
 
-        panel = sc.Panel(*panel_elements)
-        location = location_to_str(figure.figure_size.units, subplot.location)
-        panels.append(panel.move(*location))
+        location = location_to_str(fig_spec.figure_size.units, panel.location)
+        panels.append(sc.Panel(*panel_elements).move(*location))
 
     def dim2str(width_height: Union[float, int], units: Units):
         return f"{width_height:.2f}in" if units == Units.inch else f"{width_height:.2f}{units.name}"
 
-    sc.Figure(dim2str(figure.figure_size.width, figure.figure_size.units),
-              dim2str(figure.figure_size.height, figure.figure_size.units),
-              *panels).save(figure.svg_path)
+    sc.Figure(dim2str(fig_spec.figure_size.width, fig_spec.figure_size.units),
+              dim2str(fig_spec.figure_size.height, fig_spec.figure_size.units),
+              *panels).save(fig_spec.svg_path)
 
-    if figure.generate_image != ImageType.none:
+    if fig_spec.generate_image != ImageType.none:
         """ Taken from this shell script:
         #!/bin/sh
 
@@ -263,10 +265,10 @@ def composite(figure: FigureSpec, delete_png=False):
           rm $BN.png
         done
         """
-        basename = f"{figure.svg_path}".rstrip(".svg")
+        basename = f"{fig_spec.svg_path}".rstrip(".svg")
         image_name = f"{basename}.png"
-        run(f"inkscape --without-gui --export-png='{image_name}' --export-dpi {figure.image_dpi} {figure.svg_path}")
-        if figure.generate_image == ImageType.tiff:
+        run(f"inkscape --without-gui --export-png='{image_name}' --export-dpi {fig_spec.image_dpi} {fig_spec.svg_path}")
+        if fig_spec.generate_image == ImageType.tiff:
             tiff_name = f"{basename}.tiff"
             run(f"convert -compress LZW -alpha remove {image_name} {tiff_name}")
             run(f"mogrify -alpha off {tiff_name}")
@@ -288,16 +290,16 @@ def get_pts_per_unit(units: Units) -> Union[float, int]:
     return scaling_magic * pts_per_unit
 
 
-def figure_size_in_inches(fig_size: ElemSize) -> Tuple[float, ...]:
-    # Convert the subplot_size to inches, the units used by matplotlib to specify figure size.
-    if fig_size.units == Units.pt:
-        return tuple(wh / plt_ppi for wh in (fig_size.width, fig_size.height))
-    elif fig_size.units == Units.cm:
-        return tuple(wh / cm_per_inch for wh in (fig_size.width, fig_size.height))
-    elif fig_size.units == Units.mm:
-        return tuple(wh / cm_per_inch * 10 for wh in (fig_size.width, fig_size.height))
+def figure_size_in_inches(elem_size: ElemSize) -> Tuple[float, ...]:
+    # Convert the elem_size to inches, the units used by matplotlib to specify figure size.
+    if elem_size.units == Units.pt:
+        return tuple(wh / plt_ppi for wh in (elem_size.width, elem_size.height))
+    elif elem_size.units == Units.cm:
+        return tuple(wh / cm_per_inch for wh in (elem_size.width, elem_size.height))
+    elif elem_size.units == Units.mm:
+        return tuple(wh / cm_per_inch * 10 for wh in (elem_size.width, elem_size.height))
     else:
-        return tuple(wh for wh in (fig_size.width, fig_size.height))
+        return tuple(wh for wh in (elem_size.width, elem_size.height))
 
 
 def location_to_str(default_units: Units, loc: Location) -> Tuple[str, ...]:
@@ -309,7 +311,6 @@ def location_to_str(default_units: Units, loc: Location) -> Tuple[str, ...]:
 def check_svg_scale(svg: sc.SVG):
     """
     :param svg:
-    :param target_scale:
     :return:
     """
     import re
